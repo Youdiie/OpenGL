dd6c145f16ee76e562d32badad2927c635928d48
Youdiie <youjin@acon3d.com>
2023-04-10 18:11:16 +0900

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by 김유진 on 2023/04/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
