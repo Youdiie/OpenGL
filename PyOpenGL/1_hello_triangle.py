14ead6b73dfe8d560fe40233cecef06d05ea3d80
Youdiie <youjin@acon3d.com>
2023-04-16 16:52:19 +0900

    색상 추가

--- PyOpenGL/1_hello_triangle.py
+++ PyOpenGL/1_hello_triangle.py
@@ -14,6 +14,16 @@ def triangle():
     glEnd()
 
 
+# iterate() 함수가 없으면 삼각형을 그리고 화면을 초기화하는 작업이 계속 반복됨
+def iterate():
+    glViewport(0, 0, w, h)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-1.0, 1.0, -1.0, 1.0, 1.0, -1.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+
 # triangle() 함수만으론 화면에 그려지지 않음
 # 화면을 만들고 showScreen() 함수를 오버라이딩 해서 triangle() 함수를 호출해야 함
 
@@ -21,6 +31,8 @@ def triangle():
 def showScreen():  # showScreen으로 불러야 GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT가 적용됨
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # 화면 초기화
     glLoadIdentity()
+    iterate()
+    glColor3f(0.0, 0.0, 3.0)  # 색상 지정
     triangle()
     glutSwapBuffers()
 
