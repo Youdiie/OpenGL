a7d48f1360e3534ca329abf74c9302b250e12b1b
Youdiie <youjin@acon3d.com>
2023-04-24 15:44:22 +0900

    쉐이더 코드 수정

--- src/1.Getting-Started/1.3.Shaders.cpp
+++ src/1.Getting-Started/1.3.Shaders.cpp
@@ -1,23 +1,37 @@
-//
-// Created by habi on 10/17/2022.
-//
 #include <iostream>
 #include <string>
 #include <math.h>
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
-#include "../../shaders/1.Getting-Started/shader.h"
+// #include "../../shaders/1.Getting-Started/shader.h"
 
 using namespace std;
-
 
 /* Prototypes */
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos, 1.0);\n"
+                                 "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "uniform vec4 ourColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = ourColor;\n"
+                                   "}\n\0";
 
 /* Main */
-int main() {
+int main()
+{
     cout << "Run Main()" << endl;
 
     /* GLFW 초기화 */
@@ -28,44 +42,82 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     /* Window object 생성 */
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Shaders", NULL, NULL);
-    if (window == NULL) {
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Shaders", NULL, NULL);
+    if (window == NULL)
+    {
         cout << "Failed to creat GLFW window" << endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
-
-    /* Callbacks */
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     /* Initialize GLAD */
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
         cout << "Failed to initialize GLAD" << endl;
         return -1;
     }
 
-    /* Shader Settings */
-    /*
-     * CLion에서는 cmake-build-debug라는 폴더 내부에 executable 파일이 생성됨.
-     * 그래서 relative path로 vertex, fragment shader 파일들을 가져오기 위해서는 상위 폴더로 이동해야함.
-     */
-    string dirPath = "../shaders/1.Getting-Started/";
-    string vertexPath = dirPath + "shader.vs";
-    string fragmentPath = dirPath + "shader.fs";
-    Shader ourShader(vertexPath.c_str(), fragmentPath.c_str());
+    // 쉐이더 build and compile
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                  << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Shader Settings
 
     /* Set up vertex & index data */
     float vertices[] = {
-            // positions                      // colors
-            0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,         // top right
-            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,      // bottom right
-            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  // bottom left
-            -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f    // top left
+        // positions                      // colors
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // top right
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f   // top left
     };
     unsigned int indices[] = {
-            0, 1, 3,   // first triangle
-            1, 2, 3    // second triangle
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
     };
 
     /* Set up buffers */
@@ -87,10 +139,10 @@ int main() {
 
     // Linking vertex attributes pointers
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
     // glVertexAttribPointer()를 통해서 vertex attribute의 vertex buffer object를 가져왔기 때문에, unbind가 가능함
@@ -99,20 +151,22 @@ int main() {
     glBindVertexArray(0);
 
     /* Render Loop */
-    while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window))
+    {
         processInput(window);
 
-        ourShader.use();
-        ourShader.setFloat("someUniform", 1.0f);
-
+        // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // 시간에 따라 색상 변하는 부분을 비활성화
-//        float timeValue = glfwGetTime();
-//        float greenValue = sin(timeValue) / 2.0f + 0.5f;
-//        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-//        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        // glUnifrom을 호출하기 전 shaderProgram을 활성화 해야함
+        glUseProgram(shaderProgram);
+
+        // update shader uniform
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
         glBindVertexArray(VAO);
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
@@ -126,20 +180,20 @@ int main() {
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
-
     return 0;
 }
 
-
 /* Functions */
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window) {
+void processInput(GLFWwindow *window)
+{
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
-
