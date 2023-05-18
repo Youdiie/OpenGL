1b9d768c09c8fda66ede6edbd14231c2bb14c823
Youdiie <youjin@acon3d.com>
2023-05-18 17:03:29 +0900

    spotlight 추가

--- include/shaders/lighting/2.5.light_casters.fs
+++ include/shaders/lighting/2.5.light_casters.fs
@@ -10,7 +10,10 @@ struct Material {
 
 struct Light {
     vec3 position;
-    // vec3 direction; // directional light
+    vec3 direction; // directional light
+    float cutoff;
+    float outerCutOff;
+
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
@@ -30,9 +33,13 @@ uniform Light light;
 
 void main()
 {   
+    // vec3 lightDir = normalize(light.position - FragPos);
+
+    // if (theta > light.cutoff) // cosine 값이기 때문에 > 일때가 범위 내
+    // {
     // ambient
     vec3 ambient = vec3(texture(material.diffuse, TexCoords)) * light.ambient;
-    
+
     // diffuse
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos); // 빛과 fragment 사이의 방향 벡터
@@ -47,9 +54,25 @@ void main()
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess); // 반사된 빛과 뷰어 사이의 각도에 shiness를 적용
     vec3 specular = vec3(texture(material.specular, TexCoords)) * spec * light.specular;
 
+    // spotlight (soft edges)
+    float theta = dot(lightDir, normalize(-light.direction));
+    float epsilon = light.cutoff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    diffuse *= intensity;
+    specular *= intensity;
+    
+    // attenuation
     float distance = length(light.position - FragPos);
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
 
     vec3 result = (ambient + diffuse + specular) * attenuation;
     FragColor = vec4(result, 1.0);
+    // }
+    // else
+    // {
+    //     // else, use ambitent light so scene isn't completely dark outside the spotlight.
+    //     FragColor = vec4(vec3(texture(material.diffuse, TexCoords)) * light.ambient, 1.0);
+    // }
+    
+    
 }
